607d62ecc910041581342306636c9aadf5228373
Yuri <starikov.yuri@gmail.com>
2024-03-11 12:43:33 +0200

    Убираем "категории" из крошек

--- modules/int_main.php
+++ modules/int_main.php
@@ -320,7 +320,8 @@ if (count($productsArrFinal)>0) {
   <div class="col-12 col-md-9 p-0 order-2 order-md-1">
   	<div class="contentBlock maincat d-flex flex-column flex-sm-row flex-wrap">
     	<div class="productsBlock col-12 col-sm-12 pb-1 pb-sm-4">
-    		<a href="/catalog/"><div class="blockText">КАТАЛОГ ПРОДУКЦИИ</div></a>
+    	<!--	<a href="/catalog/"><div class="blockText">КАТАЛОГ ПРОДУКЦИИ</div></a> -->
+      <div class="blockText">КАТАЛОГ ПРОДУКЦИИ</div>
     		<div class="blockList maincatwrp">
     		<?php
           //foreach ($groupsArr as $row) echo $row;
@@ -339,7 +340,8 @@ if (count($productsArrFinal)>0) {
   	</div>-->
     <div class="contentBlock maincat-service d-flex flex-column flex-sm-row flex-wrap"><!-- style="display:none  !important;" -->
       <div class="productsBlock col-12 col-sm-12 pb-1 pb-sm-4">
-        <a href="/services/"><div class="blockText">КАТАЛОГ УСЛУГ</div></a>
+        <!-- <a href="/services/"><div class="blockText">КАТАЛОГ УСЛУГ</div></a> -->
+        <div class="blockText">КАТАЛОГ УСЛУГ</div>
         <div class="blockList maincatservicewrp">
           <?php
            //var_dump($categoryservices::$catarr);
@@ -359,7 +361,8 @@ if (count($productsArrFinal)>0) {
 	
 	    <div class="contentBlock maincat-service d-flex flex-column flex-sm-row flex-wrap"><!-- style="display:none  !important;" -->
       <div class="productsBlock col-12 col-sm-12 pb-1 pb-sm-4">
-        <a href="/params/"><div class="blockText">КАТАЛОГ ПРОДУКЦИИ ПО ПАРАМЕТРАМ ЗАКАЗЧИКА</div></a>
+       <!-- <a href="/params/"><div class="blockText">КАТАЛОГ ПРОДУКЦИИ ПО ПАРАМЕТРАМ ЗАКАЗЧИКА</div></a> -->
+        <div class="blockText">КАТАЛОГ ПРОДУКЦИИ ПО ПАРАМЕТРАМ ЗАКАЗЧИКА</div>
         <div class="blockList maincatservicewrp">
           <?php
 		      global $categoryparams;
